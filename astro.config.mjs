041dbcaa90dd8cb6dc7ffa818db51294803c1a3f
GenixOne <ronaldjames6548@gmail.com>
2025-08-22 09:34:15 +0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astros.zank.studio",
+	site: "https://zandkhqastro.vercel.app",
 	
 	// Add Astro's built-in i18n configuration
 	i18n: {
